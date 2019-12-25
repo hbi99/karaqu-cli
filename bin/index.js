25421704478740ab59cd62c178d3f990367171eb
Hakan Bilgin <hbi@longscript.com>
2019-12-25 18:23:22 +0100

    basic command parser

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
+"use strict";
 
-let defiant = require('../lib')
+let defiant = require("../lib")
+let aliases = defiant.aliases()
+let args = process.argv.splice(2)
+let cmd = aliases[args[0]]
 
-defiant.publish()
+if (!cmd) {
+	Object.keys(aliases).map(c =>
+		cmd ? false : aliases[c].map(a =>
+			a === args[0] ? cmd = c : false
+		))
+}
+
+// command not found - show help
+if (!cmd) return defiant.help()
+
+defiant[cmd]()
