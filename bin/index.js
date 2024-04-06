150d0fe73240c4bad81ff952f84fc8bb5890dffe
Hakan Bilgin <hbi99@hotmail.com>
2024-04-06 13:39:49 +0200

    modernizing code base

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 "use strict";
 
-let karaqu = require("../lib")
-let aliases = karaqu.aliases()
-let args = process.argv.splice(2)
-let cmd = aliases[args[0]] ? args[0] : false
+import * as karaqu from "../lib/src/index.mjs"
+
+let aliases = karaqu.aliases();
+let args = process.argv.splice(2);
+let cmd = aliases[args[0]] ? args[0] : false;
 
 if (!cmd) {
 	Object.keys(aliases).map(c =>
 		cmd ? false : aliases[c].map(a =>
 			a === args[0] ? cmd = c : false
-		))
+		));
 }
 
 // command not found - show help
-if (!cmd) return karaqu.help()
-
+if (!cmd) karaqu.help();
 // call function and pass on arguments
-karaqu[cmd].apply(karaqu, args.slice(1))
+else karaqu[cmd].apply(karaqu, args.slice(1));
--- lib/default-app/README.md
+++ lib/default-app/README.md
@@ -1,1 +1,2 @@
 # {{name}}
+<img src="public/icon.svg" width=200 height=200>
